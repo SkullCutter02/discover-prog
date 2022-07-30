459ebf7a0f623ba67394233b9d34994f92b3e87c
skullcutter02 <=>
2022-07-30 20:37:42 +0800

    Fetched all artist necessary information

--- api/src/scraper/scraper.service.ts
+++ api/src/scraper/scraper.service.ts
@@ -3,10 +3,11 @@ import Crawler from "crawler";
 
 @Injectable()
 export class ScraperService {
-  artists: string[] = [];
+  artists = [];
 
   scrapeArtists() {
     let currentId = 1;
+    let errorCount = 0; // if errorCount > 4, that means there have been 5 consecutive nonexistent artists, and therefore stop the process of scraping
 
     const artistCrawler = new Crawler({
       maxConnections: 10,
@@ -21,13 +22,18 @@ export class ScraperService {
 
           if (error && error === "error '80020009'") {
             console.log(`The artist with ID ${currentId} doesn't exist`);
+            errorCount++;
           } else {
-            const artistName = $("h1").text();
-            this.artists.push(artistName);
-            console.log(this.artists);
+            errorCount = 0;
+
+            const name = $("h1").first().text();
+            const biography = $("#moreBio").text();
+            const country = $("#main > div > h2").first().text().split(" • ")[1];
+            const imageUrl = $("#main > div > div > div > img").first().attr("src");
+            console.log(imageUrl);
           }
 
-          if (currentId <= 20) {
+          if (currentId <= 30 && errorCount < 5) {
             currentId++;
             artistCrawler.queue(`https://www.progarchives.com/artist.asp?id=${currentId}`);
           }
