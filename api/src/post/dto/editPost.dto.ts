e98865c35037364cf44bd45e6be424d9cad1fa3f
skullcutter02 <=>
2022-07-30 19:57:36 +0800

    Removed post modules

--- api/src/post/dto/editPost.dto.ts
+++
@@ -1,5 +0,0 @@
-// import { PartialType } from "@nestjs/mapped-types";
-//
-// import { CreatePostDto } from "./createPost.dto";
-//
-// export class EditPostDto extends PartialType(CreatePostDto) {}
