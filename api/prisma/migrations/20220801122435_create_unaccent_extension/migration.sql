46fbe5c4eb29068348aa254290c0a4dc955e8e09
skullcutter02 <=>
2022-08-01 20:30:12 +0800

    Unaccented search system

---
+++ api/prisma/migrations/20220801122435_create_unaccent_extension/migration.sql
@@ -0,0 +1,1 @@
+CREATE EXTENSION IF NOT EXISTS unaccent; -- This is an empty migration.
--- api/src/search/search.service.ts
+++ api/src/search/search.service.ts
@@ -11,23 +11,23 @@ export class SearchService {
 
     return this.prisma.$queryRaw`
       SELECT *, (
-          CASE
-              WHEN type = 'artist' THEN (name <-> ${query})
+          CASE 
+              WHEN type = 'artist' THEN (unaccent(name) <-> ${query})
               WHEN type = 'album' THEN
                   CASE
-                      WHEN artist_name <-> ${query} < 0.5 THEN (artist_name <-> ${query}) + 0.01
-                      WHEN artist_name <-> ${query} >= 0.5 THEN (name <-> ${query}) + 0.05
+                      WHEN unaccent(artist_name) <-> ${query} < 0.5 THEN (unaccent(artist_name) <-> ${query}) + 0.01
+                      WHEN unaccent(artist_name) <-> ${query} >= 0.5 THEN (unaccent(name) <-> ${query}) + 0.05
                   END
           END
       ) AS rank
       FROM
-          (SELECT 'artist' AS type, artists.name FROM artists) artists
+          (SELECT 'artist' AS type, artists.* FROM artists) artists
               NATURAL FULL JOIN
-          (SELECT 'album' AS type, albums.name, a.name AS artist_name FROM albums
+          (SELECT 'album' AS type, albums.*, a.name AS artist_name FROM albums
               INNER JOIN artists a on albums."artistId" = a.id) albums
-      WHERE name ILIKE ${ilike} 
-         OR (name <-> ${query}) < 0.7
-         OR (artist_name <% ${query}) IS TRUE
+      WHERE unaccent(name) ILIKE ${ilike} 
+         OR (unaccent(name) <-> ${query}) < 0.7
+         OR (unaccent(artist_name) <% ${query}) IS TRUE
       ORDER BY rank; 
     `;
   }
