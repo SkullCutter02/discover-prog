875dbba456636ed0ba3cfc3c16c56c25bb8c284e
skullcutter02 <=>
2022-08-17 22:17:22 +0800

    Created get review route

--- api/src/review/review.controller.ts
+++ api/src/review/review.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
-import { User } from "@prisma/client";
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Query, UseGuards } from "@nestjs/common";
+import { Prisma, User } from "@prisma/client";
 
 import { JwtAuthGuard } from "../auth/guards/jwt.guard";
 import { GetUser } from "../decorators/getUser.decorator";
 import { CreateReviewDto } from "./dto/createReview.dto";
 import { ReviewService } from "./review.service";
+import { ParseIncludeQueryPipe } from "../pipes/parseIncludeQuery.pipe";
 
 @Controller("review")
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @Get("/:id")
+  getReview(
+    @Param("id", ParseUUIDPipe) reviewId: string,
+    @Query("include", ParseIncludeQueryPipe) include: Prisma.ReviewInclude
+  ) {
+    return this.reviewService.findById(reviewId, include);
+  }
 
   @Post()
   @UseGuards(JwtAuthGuard)
--- api/src/review/review.service.ts
+++ api/src/review/review.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
+
 import { PrismaService } from "../prisma/prisma.service";
 import { CreateReviewDto } from "./dto/createReview.dto";
 
 @Injectable()
 export class ReviewService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  findById(reviewId: string, include?: Prisma.ReviewInclude) {
+    return this.prismaService.review.findUnique({
+      where: { id: reviewId },
+      include,
+    });
+  }
 
   create({ albumId, ...data }: CreateReviewDto, userId: string) {
     return this.prismaService.review.create({
