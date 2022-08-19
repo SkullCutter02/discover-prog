7d78860f55872ef4e13e74be17081ef2cf37f345
skullcutter02 <=>
2022-08-19 22:41:21 +0800

    Fetched genre and artist within get most popular album query

--- client/features/album/api/getMostPopularAlbums.ts
+++ client/features/album/api/getMostPopularAlbums.ts
@@ -1,7 +1,7 @@
 import { axios } from "../../../lib/axios";
 
 const getMostPopularAlbums = async (page: number, limit: number = 100) => {
-  const { data } = await axios.get(`album/most-popular?page=${page}&limit=${limit}`);
+  const { data } = await axios.get(`album/most-popular?page=${page}&limit=${limit}&include=artist,genre`);
 
   return data;
 };
