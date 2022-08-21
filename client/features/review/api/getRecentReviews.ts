b99b150e9cafcdcac885605c3baa42c4a22fd502
skullcutter02 <=>
2022-08-21 15:38:29 +0800

    Finished album preview on reviews

--- client/features/review/api/getRecentReviews.ts
+++ client/features/review/api/getRecentReviews.ts
@@ -2,7 +2,9 @@ import { axios } from "../../../lib/axios";
 import Review from "../types/review.interface";
 
 const getRecentReviews = async (page: number, limit: number) => {
-  const { data } = await axios.get<Review[]>(`review/latest?page=${page}&limit=${limit}&include=album,user`);
+  const { data } = await axios.get<Review[]>(
+    `review/latest?page=${page}&limit=${limit}&include=album,album.artist,user`
+  );
 
   return data;
 };
--- client/features/review/components/ReviewAlbumCard.tsx
+++ client/features/review/components/ReviewAlbumCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Album from "../../album/types/album.interface";
-import { AspectRatio, Box, Image } from "@chakra-ui/react";
+import { AspectRatio, Box, Image, Text, Flex } from "@chakra-ui/react";
 
 interface Props {
   album: Album;
@@ -9,10 +9,28 @@ interface Props {
 const ReviewAlbumCard: React.FC<Props> = ({ album }) => {
   return (
     <>
-      <Box float={"left"} bg={"white"} p={3} w={"200px"} mr={4}>
+      <Box float={"left"} bg={"white"} p={3} w={"30%"} mr={4}>
         <AspectRatio w={"full"} ratio={1}>
           <Image src={`https://www.progarchives.com/${album.imageUrl}`} />
         </AspectRatio>
+        <Flex flexDir={"column"} align={"center"}>
+          <Text
+            fontSize={14}
+            mt={2}
+            color={"primary"}
+            textDecoration={"underline"}
+            cursor={"pointer"}
+            fontWeight={600}
+          >
+            {album.name}
+          </Text>
+          <Text mt={0.5} fontSize={13}>
+            by{" "}
+            <Box as={"span"} textTransform={"uppercase"} textDecoration={"underline"} cursor={"pointer"}>
+              {album.artist.name}
+            </Box>
+          </Text>
+        </Flex>
       </Box>
     </>
   );
