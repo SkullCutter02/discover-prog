9341d14b97f76e61d2d4e34c98460d53926c9626
skullcutter02 <=>
2022-08-22 20:27:46 +0800

    Displayed custom error message when the user logs in using invalid credentials

--- api/src/auth/guards/local.guard.ts
+++ api/src/auth/guards/local.guard.ts
@@ -1,3 +1,15 @@
 import { AuthGuard } from "@nestjs/passport";
+import { UnauthorizedException } from "@nestjs/common";
 
-export class LocalAuthGuard extends AuthGuard("local") {}
+export class LocalAuthGuard extends AuthGuard("local") {
+  constructor() {
+    super();
+  }
+
+  handleRequest(err: any, user: any) {
+    if (err || !user) {
+      throw new UnauthorizedException("Invalid credentials");
+    }
+    return user;
+  }
+}
