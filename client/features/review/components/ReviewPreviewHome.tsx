dc87621e8990eecbe54fe6487b837a0eff17f194
skullcutter02 <=>
2022-08-22 18:56:58 +0800

    Used wrap instead of flex for review ratings in home page

--- client/features/review/components/ReviewPreviewHome.tsx
+++ client/features/review/components/ReviewPreviewHome.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 
 import Star from "../../../components/widgets/Star";
 import Review from "../types/review.interface";
@@ -14,18 +14,20 @@ const ReviewPreviewHome: React.FC<Props> = ({ review }) => {
     <>
       <Box bg={"#efefef"} overflow={"hidden"} mt={4} p={5}>
         <ReviewAlbumCard album={review.album} />
-        <Flex>
-          <Text fontWeight={600} mr={3}>
-            Review by {review.user.name}
-          </Text>
-          <HStack spacing={2}>
-            {Array(5)
-              .fill("")
-              .map((_, i) => (
-                <Star key={review.id + review.user.id + i} isFilled={i < review.rating} />
-              ))}
-          </HStack>
-        </Flex>
+        <Wrap spacing={3} align={"center"}>
+          <WrapItem>
+            <Text fontWeight={600}>Review by {review.user.name}</Text>
+          </WrapItem>
+          <WrapItem>
+            <HStack spacing={2}>
+              {Array(5)
+                .fill("")
+                .map((_, i) => (
+                  <Star key={review.id + review.user.id + i} isFilled={i < review.rating} />
+                ))}
+            </HStack>
+          </WrapItem>
+        </Wrap>
         <Text fontSize={14} whiteSpace={"pre-line"} mt={3}>
           {review.body}
         </Text>
